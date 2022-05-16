b6905875c57397491649a4f7c4bdbe7a959b85cb
Emile Gaston <eng1u18@soton.ac.uk>
2022-05-16 16:51:16 +0100

    Added classification, model testing and output to dataframe with classification TODO: Output to report.txt, LP scheduling

--- modeltesting.py
+++ modeltesting.py
@@ -1,50 +1,56 @@
 # https://stackabuse.com/overview-of-classification-methods-in-python-with-scikit-learn/
 # Begin by importing all necessary libraries
-import pandas
 import pandas as pd
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("support/TrainingData.txt", header=None)
-print("training data : ")
-training_data = pd.read_csv("support/TrainingData.txt", header=None)
-print(training_data)
-print("Testing data: ")
-testing_data = pd.read_csv("support/TestingData.txt", header=None)
-print(testing_data)
+
 # It is a good idea to check and make sure the data is loaded as expected.
 
+print(data.head(5))
+
+
 # # Pandas ".iloc" expects row_indexer, column_indexer
+X = data.iloc[:, :-1].values
 # # Now let's tell the dataframe which column we want for the target/labels.
+y = data[24]
 #print(X[:5])
 #print(y[:5])
 # # Test size specifies how much of the data you want to set aside for the testing set.
 # # Random_state parameter is just a random seed we can use.
 # # You can use it if you'd like to reproduce these specific results.
-x_training = training_data.iloc[:, :-1]
-y_training = training_data.iloc[:, -1:]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
 print("testing the SVC model")
 SVC_model = SVC()
 # training
-SVC_model.fit(x_training, y_training)
+SVC_model.fit(X_train, y_train)
 # prediction
-
-SVC_prediction = SVC_model.predict(testing_data)
-print("SVCPredictions: ")
-print(SVC_prediction)
-
-testingframe = pandas.DataFrame(SVC_prediction, columns=["normal"])
-testedframe = pandas.concat([testing_data,testingframe], axis=1)
-print(testedframe)
-abnormalframe = testedframe.loc[testedframe['normal'] == 1]
-print("abnormalframe: ")
-abnormalframe = abnormalframe.iloc[:, :-1]
-print(abnormalframe.values.tolist())
+print("X_test values: ")
+print(X_test)
+SVC_prediction = SVC_model.predict(X_test)
+#  Accuracy score is the simplest way to evaluate
+print("Accuracy score for SVC: ", accuracy_score(SVC_prediction, y_test))
 
 
+print("testing the KNN model")
+
+# KNN model requires you to specify n_neighbors,
+# the number of points the classifier will look at to determine what class a new point belongs to
+KNN_model = KNeighborsClassifier(n_neighbors=3)
+# training
+KNN_model.fit(X_train, y_train)
+# prediction
+KNN_prediction = KNN_model.predict(X_test)
+
+print("Accuracy score for KNN: ", accuracy_score(KNN_prediction, y_test))
+
+# But Confusion Matrix and Classification Report give more details about performance
+# print(confusion_matrix(SVC_prediction, y_test))
+# print(classification_report(KNN_prediction, y_test))
